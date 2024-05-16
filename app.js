595ea612322d6db7fb79cbcedf291acb58f0977d
jankrum <professionalninety@gmail.com>
2024-05-16 14:38:59 -0700

    Made loading a chart and script work

--- app.js
+++ app.js
@@ -4,97 +4,75 @@ import Timeline from './timeline.js';  // Used to control playback
 // Get the output to the synthesizer
 const synthesizerOutput = await getConnection('synthesizer', 'outputs');
 
-
-// Selectors for the HTML elements
-const SELECTORS = {
-    playButton: '#play-button',
-    pauseButton: '#pause-button',
-    resumeButton: '#resume-button',
-    stopButton: '#stop-button',
-    chanceSpan: '#chanceSpan',
-    knob: '#knob'
-};
-
 const timeline = new Timeline(
-    document.querySelector(SELECTORS.playButton),
-    document.querySelector(SELECTORS.pauseButton),
-    document.querySelector(SELECTORS.resumeButton),
-    document.querySelector(SELECTORS.stopButton),
+    document.querySelector('#play-button'),
+    document.querySelector('#pause-button'),
+    document.querySelector('#resume-button'),
+    document.querySelector('#stop-button'),
     synthesizerOutput
 );
 
-// Update the chance span when the knob is moved
-document.querySelector(SELECTORS.knob).addEventListener('input', e => {
-    document.querySelector(SELECTORS.chanceSpan).innerText = e.target.value;
-});
-
-timeline.setTempo(200);
-
-function makeOctaveJumpNote(baseNote) {
-    const roll = Math.random() * 100;
-    const knobInput = document.querySelector(SELECTORS.knob);
-    return () => {
-        const knobValue = knobInput.value;
+async function load() {    
+    const chart = await (async () => {
+        const rawChart = await fetch('./static/twinkle-twinkle-little-star.json').then(response => response.json());
         
-        console.log(`Roll: ${roll}, Knob: ${knobValue}`);
-
-        return roll > knobValue ? baseNote : baseNote + 12;
+        function convertPitchNameToMidiNumber(pitchName) {
+            const letters = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+            
+            const letter = pitchName.slice(0, -1);
+            const octave = Number(pitchName.at(-1));
+            
+            const pitchNameIndex = letters.indexOf(letter);
+            return pitchNameIndex + ((octave + 2) * 12);
+        }
+        
+        return {
+            tempo: Number(rawChart.tempo),
+            notes: rawChart.notes.map(note => ({
+                pitch: convertPitchNameToMidiNumber(note.pitch),
+                start: Number(note.start),
+                duration: Number(note.duration)
+            }))
+        };
+    })();
+    
+    const script = await fetch('./static/octave-jumper.js').then(response => response.text());
+    
+    const controller = (() => {
+        const prefixSpan = document.querySelector('span#prefix');
+        const valueSpan = document.querySelector('span#value');
+        const suffixSpan = document.querySelector('span#suffix');
+        
+        const input = document.querySelector('input');
+        
+        return {
+            configure: (prefix, suffix) => {
+                prefixSpan.textContent = prefix;
+                suffixSpan.textContent = suffix;
+                
+                function updateValue() {
+                    valueSpan.textContent = input.value;
+                }
+                
+                input.addEventListener('input', updateValue);
+                
+                updateValue();
+                
+                return input
+            }
+        };
+    })();
+    
+    const newChart = eval(script)(chart);
+    console.log(newChart.notes[0].pitch());
+    
+    timeline.setTempo(newChart.tempo);
+    
+    for (const note of newChart.notes) {
+        timeline.addNoteToChart(note.pitch, note.start, note.duration);
     }
 }
 
-const twinkleTwinkleLittleStarChart = [
-    { start: 0, length: 0.9, note: makeOctaveJumpNote(60)},
-    { start: 1, length: 0.9, note: makeOctaveJumpNote(60)},
-    { start: 2, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 3, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 4, length: 0.9, note: makeOctaveJumpNote(69)},
-    { start: 5, length: 0.9, note: makeOctaveJumpNote(69)},
-    { start: 6, length: 1.9, note: makeOctaveJumpNote(67)},
-    { start: 8, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 9, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 10, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 11, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 12, length: 0.9, note: makeOctaveJumpNote(62)},
-    { start: 13, length: 0.9, note: makeOctaveJumpNote(62)},
-    { start: 14, length: 1.9, note: makeOctaveJumpNote(60)},
-    { start: 16, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 17, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 18, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 19, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 20, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 21, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 22, length: 1.9, note: makeOctaveJumpNote(62)},
-    { start: 24, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 25, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 26, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 27, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 28, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 29, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 30, length: 1.9, note: makeOctaveJumpNote(62)},
-    { start: 32, length: 0.9, note: makeOctaveJumpNote(60)},
-    { start: 33, length: 0.9, note: makeOctaveJumpNote(60)},
-    { start: 34, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 35, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 36, length: 0.9, note: makeOctaveJumpNote(69)},
-    { start: 37, length: 0.9, note: makeOctaveJumpNote(69)},
-    { start: 38, length: 1.9, note: makeOctaveJumpNote(67)},
-    { start: 40, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 41, length: 0.9, note: makeOctaveJumpNote(65)},
-    { start: 42, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 43, length: 0.9, note: makeOctaveJumpNote(64)},
-    { start: 44, length: 0.9, note: makeOctaveJumpNote(62)},
-    { start: 45, length: 0.9, note: makeOctaveJumpNote(67)},
-    { start: 46, length: 4, note: makeOctaveJumpNote(60)}
-];
-
-for (const { start, length, note } of twinkleTwinkleLittleStarChart) {
-    timeline.addNoteToChart(note, start, length);
-}
-
-// Clear local storage when the Konami code is entered
-new Konami(() => {
-    localStorage.clear();
-    alert('Cleared local storage!');
-});
-
+await load();
+    
 console.log('%cReady!', 'background-color: green; color: white;');
--- static/octave-jumper.js
+++ static/octave-jumper.js
@@ -1,5 +1,24 @@
-const octaveJumpChance = CONTROLLER.configure('8va chance: ', 0, 100, '%');
+const octaveJumpChanceControl = controller.configure('8va chance: ', '%');
 
-return function (state, action) {
-    // reducer function goes here
-};
+function makeOctaveJumpNote(basePitch) {
+    const roll = Math.random() * 100;
+    return () => {
+        const chance = octaveJumpChanceControl.value;
+
+        console.log(`Roll: ${roll}, chance: ${chance}`);
+
+        return basePitch + (roll > chance ? 0 : 12);
+    }
+}
+
+function transformer(chart) {
+    const {tempo, notes} = chart;
+    const newNotes = notes.map(note => {
+        const {pitch, start, duration} = note;
+        const newPitch = makeOctaveJumpNote(pitch);
+        return { pitch: newPitch, start, duration};
+    });
+    return {tempo, notes: newNotes};
+}
+
+transformer;
